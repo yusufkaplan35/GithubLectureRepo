b18d7106144a6b930c171860211f8afa82015dfd
Yusuf Kaplan <yusufkaplan3591@gmail.com>
2023-08-19 20:39:18 +0300

    Version 3

--- src/main/java/Class01.java
+++ src/main/java/Class01.java
@@ -1,15 +1,20 @@
 public class Class01 {
     public static void main(String[] args) {
 
-        //Versiyon1
+        //Versiyon 1
 
         int x=1;
         int y=2;
         System.out.println(x+y);
 
-        //Versiyon2
+        //Versiyon 2
         int z=5;
         System.out.println(x+y+z);
+
+        //Versiyon 3
+        System.out.println("Vesiyon 3 için kodlar");
+
+
 
     }
 }
